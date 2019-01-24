fcb372885bd2b3091944dcb429af8f76918d935f
NeuTrix <neutrix@brandgeneering.com>
2019-01-23 18:34:25 -0800

    Add Comments Model and migrations
    Test passing

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :commentable
+  has_many :comments, as: :commentable
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_015703) do
+ActiveRecord::Schema.define(version: 2019_01_24_022921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "commentable_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_01_24_015703) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
