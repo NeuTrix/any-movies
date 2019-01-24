f999a4491f9a6f1ecacb5f1808d6d8b7317b9cf9
NeuTrix <neutrix@brandgeneering.com>
2019-01-23 19:56:42 -0800

    Comments and models complete.  Seed db passing test
    Hot Comments on Comments action!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
-@user = User.create(username: "mickey").save
+@user = User.create(username: "mickey")
+@user.save
 
-@movie = Movie.create(title: 'alien').save
+@critic = User.create(username: "goofy")
+
+@movie = Movie.create(title: 'alien')
+@movie.save
+
+@comment = @user.comments.build(
+  commentable_id: @movie.id,
+  commentable_type: @movie.class.name,
+  body: "One of my all time favorites, Go Ripley!!",
+  rating: 5
+)
+@comment.save
+ 
+@response = @critic.comments.build(
+  commentable_id: @comment.id,
+  commentable_type: @comment.class.name,
+  body: "Well, I actually preferred the action in Camerons Aliens
+  Much more credible and interesteing",
+  rating: 5
+)
+@response.save
