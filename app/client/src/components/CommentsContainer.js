1d6ea5ebfb27ee804a18d31584db8e0f43b94208
NeuTrix <neutrix@brandgeneering.com>
2019-01-25 14:43:31 -0800

    API Proxy Server Connects!
    Had the setupProxy file in the wrong directory (should be in 'src')
    
    Proxy call works fine

--- app/client/src/components/CommentsContainer.js
+++ app/client/src/components/CommentsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-// import axios from 'axios'
+import axios from 'axios'
 
 // include props declartaions (classes)
 
@@ -9,18 +9,48 @@ class CommmentsContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      comments: [],
     }
   }
 
   // comp will mount to capture the incoming data
   // implement redux to hold onto state
+  // componentDidMount(){
+  //   axios.get('api/comments')
+  //   .then( resp => {
+  //     console.log(resp)
+  //     this.setState({comments: resp.data})
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+
+   componentDidMount() {
+     axios.get('api/comments')
+       .then(resp => {
+         console.log("it worked?");
+         this.setState({
+           comments: resp.data
+         });
+       })
+       .catch(err => console.log(err))
+   }
 
   render() {
     const { classes } = this.props
+    const { comments } = this.state
+
+    const commentsList = comments.map(com => {
+     return (
+        <li key={com.id}>
+          {com.body}
+        </li>
+      )}
+    )
+    
     return (
       <div className={classes.grid}>
         <h1> Comments Container </h1>
+        <ul> {commentsList} </ul>
       </div>
     )
   }
--- app/client/src/components/CommentsList.js
+++ app/client/src/components/CommentsList.js
@@ -1,46 +1,46 @@
-// // to provide a list of comments for main display area
-// import React, {
-//   Component
-// } from 'react';
-// import PropTypes from 'prop-types';
-// import {
-//   withStyles
-// } from '@material-ui/core/styles';
-// // import axios from 'axios'
+// to provide a list of comments for main display area
+import React, {
+  Component
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+  withStyles
+} from '@material-ui/core/styles';
+// import axios from 'axios'
 
-// // include props declartaions (classes)
+// include props declartaions (classes)
 
-// class CommmentsContainer extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
+class CommmentsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
 
-//     }
-//   }
+    }
+  }
 
-//   // comp will mount to capture the incoming data
-//   // implement redux to hold onto state
+  // comp will mount to capture the incoming data
+  // implement redux to hold onto state
 
-//   render() {
-//     const {
-//       classes
-//     } = this.props
-//     return ( <
-//       div className = {
-//         classes.grid
-//       } >
-//       <
-//       h1 > Hello < /h1> <
-//       /div>
-//     )
-//   }
-// }
+  render() {
+    const {
+      classes
+    } = this.props
+    return ( <
+      div className = {
+        classes.grid
+      } >
+      <
+      h1 > Hello < /h1> <
+      /div>
+    )
+  }
+}
 
-// const styles = {
-//   grid: {
-//     display: 'grid',
-//     gridAreas: `"list"`
-//   }
-// }
+const styles = {
+  grid: {
+    display: 'grid',
+    gridAreas: `"list"`
+  }
+}
 
-// export default withStyles(styles)(CommmentsContainer)
+export default withStyles(styles)(CommmentsContainer)
