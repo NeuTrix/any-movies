da3c333975d55c247a25af346101f944fd0f953f
NeuTrix <neutrix@brandgeneering.com>
2019-01-27 16:29:14 -0800

    Movie OMDB api call woking an data setting
    Mocking the imdbID in the get call

--- app/client/src/components/MovieContainer.js
+++ app/client/src/components/MovieContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 
+
 // include props declartaions (classes)
 const propTypes = {
   classe: PropTypes.instanceOf(Object).isRequired,
@@ -12,33 +13,61 @@ class MovieContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      movie: {},
+      apiKey: process.env.REACT_APP_OMBD_MOVIE_API_KEY,
+      movie: { 
+        Title: "Placeholder",
+        imdbID: 'tt0078748',
+      },
     }
+    this.getMovieData = this.getMovieData.bind(this);
   }
 
-  // componentDidMount() {
-  //   const { movie } = this.state;
-  //   // should work from OMDB api vs my api
-  //   axios.get(`api/movies/${movie.id}`)
-  //     .then(resp => {
-  //       console.log(resp);
-  //       this.setState({
-  //         movie: resp.data
-  //       });
-  //     })
-  //     .catch(err => console.log(err))
-  // }
+  componentDidMount() {
+    const { movie } = this.state;
+    // should work from OMDB api vs my api
+    // axios.get(`api/movies/${movie.id}`)
+    //   .then(resp => {
+    //     console.log(resp);
+    //     this.setState({
+    //       movie: resp.data
+    //     });
+    //   })
+    //   .catch(err => console.log(err))
+  }
+
+  getMovieData() {
+    const { apiKey, movie } = this.state;
+
+
+    axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+      .then(resp => {
+        console.log(resp);
+        this.setState({ movie: resp.data});
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
+
+
 
   render() {
     const { classes } = this.props
-    // const { movie } = this.state
+    const { movie } = this.state
 
     return(
       <div className={classes.main} >
-        <div style={{gridArea: 'mTitle'}} >t</div> 
-        <div style={{gridArea: 'mYear'}} >y</div> 
+        <div style={{gridArea: 'mTitle'}} >
+          { movie.Title }
+        </div> 
+        <div style={{gridArea: 'mYear'}} >
+          <button onClick={this.getMovieData} > test </button>
+        </div> 
         <div style={{gridArea: 'mPoster'}} >p</div> 
-        <div style={{gridArea: 'mDescr'}} >d</div> 
+        <div style={{gridArea: 'mDescr'}} >
+        
+        </div> 
         <div style={{gridArea: 'mLetter'}} >l</div> 
         <div style={{gridArea: 'cRating'}} >r</div> 
       </div>
