7dbcebd8cf58822c5cc045ea4cb44e16fd1d8797
NeuTrix <neutrix@brandgeneering.com>
2019-01-27 20:18:15 -0800

    update index file to remove Redux

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -3,36 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// connect Redux
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
-<<<<<<< HEAD
-// set initial state
-const initialState = {
-  movie: { id: 1 }
-};
-
-// simple reducer to support te store
-function reducer(state=initialState, action) {
- return state;
-}
-
-const store = createStore(reducer);
-
-// store.dispatch({}) //some actions
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-
-  document.getElementById('root')
-=======
 ReactDOM.render(
     <App />
   , document.getElementById('root')
->>>>>>> integrateOMDB
 );
 
 // If you want your app to work offline and load faster, you can change
