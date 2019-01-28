38c49aa3b88f07164c445e06f88b01d9bd5c3a12
NeuTrix <neutrix@brandgeneering.com>
2019-01-28 02:42:12 -0800

    FIx yarn start error: "Unexpected token { error Command failed with exit code 1."
    Remove the import function from the setupProxy.js file.
    
    Tried to import a helper but drove this issue instead.

--- app/client/src/setupProxy.js
+++ app/client/src/setupProxy.js
@@ -1,9 +1,7 @@
-import {url_local_api} from './helpers/api.helper';
-
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(proxy('/api', 
-    { target: `${url_local_api}` }
+    { target: `http://localhost:3001` }
   ));
 };
