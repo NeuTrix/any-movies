4b459128cb835b8ed305c1bf3a3219a4ea6bcdc5
NeuTrix <neutrix@brandgeneering.com>
2019-01-28 10:57:38 -0800

    change movies yml and update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,74 +13,74 @@
 @critic.password_confirmation = 'bananaBro'
 @critic.save
 
-# create some movies
-@movie = Movie.create(
+# create some aliens
+@alien = Movie.create(
   title: 'Alien', 
   imdb_id: 'tt0078748'
   )
-@movie.save
+@alien.save
 
-@movie2 = Movie.create(
+@starwars = Movie.create(
   title: 'Star Wars',
   imdb_id: 'tt0076759'
   )
-@movie2.save
+@starwars.save
 
-# comments of first user on both movies
-@comment = @movie.comments.build(
+# comments of first user on both aliens
+@comment_alien = @alien.comments.build(
   title: "ALIEN: Ripley Rocks iT!",
   author: @user.username,
   user_id: @user.id,
-  commentable_id: @movie.imdb_id,
-  commentable_type: @movie.class.name,
+  commentable_id: @alien.imdb_id,
+  commentable_type: @alien.class.name,
   body: "One of my all time favorites, Go Ripley!!",
   rating: 4
 )
-@comment.save
+@comment_alien.save
 
-@comment2 = @movie.comments.build(
+@comment_starwars = @starwars.comments.build(
   title: "STAR WARS: As romantic as the universe",
   author: @user.username,
   user_id: @user.id,
-  commentable_id: @movie2.imdb_id,
-  commentable_type: @movie2.class.name,
-  body: "I had my first kiss at this movie.  
+  commentable_id: @starwars.imdb_id,
+  commentable_type: @starwars.class.name,
+  body: "I had my first kiss at this alien.  
         Yes, it was the 70's, but who's counting?",
   rating: 5
 )
-@comment2.save
+@comment_starwars.save
  
 # comments of second user, on those Comments
-@response = @comment2.comments.build(
+@response_alien = @comment_alien.comments.build(
   title: "Cameron rocks it better",
   author: @critic.username,
   user_id: @critic.id,  
-  commentable_id: @comment2.id,
-  commentable_type: @comment2.class.name,
+  commentable_id: @comment_alien.id,
+  commentable_type: @comment_alien.class.name,
   body: "Well, I actually preferred the action in Camerons Aliens
   Much more credible and interesteing",
   rating: 4
 )
-@response.save
+@response_alien.save
 
-@response2 = @response.comments.build(
+@response2_alien = @response_alien.comments.build(
   title: "Yuck",
   author: @critic.username,
   user_id: @critic.id,
-  commentable_id: @response.id,
-  commentable_type: @response.class.name,
+  commentable_id: @response_alien.id,
+  commentable_type: @response_alien.class.name,
   body: "I had NO idea it was that old.  Old people kiss :(|)",
   rating: 3
 )
-@response2.save
+@response2_alien.save
 
 
-# test to see if a comment can be built from a movie
-@testComment = @movie.comments.build(
-  title: "can you comment on a movie dircetly?",
-  commentable_id: @movie.imdb_id,
-  commentable_type: @movie.class.name,
-  body: "I'm hopint to comment on this film directly",
+# test to see if a comment can be built from a alien
+@testComment = @alien.comments.build(
+  title: "can you comment on a alien dircetly?",
+  commentable_id: @alien.imdb_id,
+  commentable_type: @alien.class.name,
+  body: "I'm hoping to comment on this film directly",
   rating: 5
 )
 
