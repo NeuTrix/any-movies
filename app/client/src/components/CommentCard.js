72a0e48f341b9bf88902d34f9069336bcbbcd424
NeuTrix <neutrix@brandgeneering.com>
2019-01-29 08:50:34 -0800

    make comment card

--- app/client/src/components/CommentCard.js
+++ app/client/src/components/CommentCard.js
@@ -7,58 +7,52 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import CommentContainer from './CommentableContainer';
+
+const propTypes = {
+  classes: PropTypes.instanceOf(Object).isRequired,
+  comment: PropTypes.instanceOf(Object).isRequired,
+}
+
+function SimpleCard(props) {
+  const { classes, comment } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="subtitle" component="h2">
+          { comment.title}
+        </Typography>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          by: { comment.author }
+        </Typography>
+        <Typography variant="body1" component="p">
+          { comment.body }
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">see comments</Button>
+      </CardActions>
+      <CardActions className={classes.btnRight} >
+        <Button size="small">reply</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
 const styles = {
   card: {
+    // gridTemplateAreas
     minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
   },
   pos: {
     marginBottom: 12,
   },
 };
 
-function SimpleCard(props) {
-  const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
-
-  return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
-}
-
-SimpleCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+SimpleCard.propTypes = propTypes;
 
 export default withStyles(styles)(SimpleCard);
