2be741885736ea2b0197dc0836aca6866cb45619
NeuTrix <neutrix@brandgeneering.com>
2019-01-30 20:44:40 -0800

    Pass curr_user prop from App.js.
    Will set inital state in the MovieContainer page.  App not stictched

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 // pass in state to the MovieReveiwPage via Redux
 import './App.css';
-import HomeContainer from './components/Home/HomeContainer';
+import MovieContainer from './components/Home/MovieContainer';
 // add @material UI theme provider
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import masterTheme from './helpers/masterTheme';
 
-  
+const curr_user = {
+  userName: "Dantastic3339",
+  userId: 1
+}
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={masterTheme}>  
         <div className="App">
-          <HomeContainer/>
+          <MovieContainer
+            curr_user={curr_user}
+          />
         </div>
       </MuiThemeProvider>
     );
