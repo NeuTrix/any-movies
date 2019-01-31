feaa3cb66e5e3feb57c9f460713302bb6346b6d2
NeuTrix <neutrix@brandgeneering.com>
2019-01-30 16:38:21 -0800

    Update HomeContainer and HomePage llinks

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // pass in state to the MovieReveiwPage via Redux
 import './App.css';
-import HomeContainer from './components/HomeContainer';
+import HomeContainer from './components/Home/HomeContainer';
 // add @material UI theme provider
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import masterTheme from './helpers/masterTheme';
