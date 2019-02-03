bf711d955a6d22480b5992142148757ff6868b35
NeuTrix <neutrix@brandgeneering.com>
2019-02-03 11:01:52 -0800

    test apikey

--- client/src/helpers/api.helper.js
+++ client/src/helpers/api.helper.js
@@ -5,3 +5,5 @@ export const url_local_api = `http://localhost:3001`;
 // utilize CRA's Env variables (start with 'REACT_APP')
 export const apiKey = process.env.REACT_APP_OMBD_MOVIE_API_KEY
 export const url_movie_data = `http://www.omdbapi.com/?apikey=${apiKey}`;
+console.log('--->', apiKey)
+console.log('--->', url_movie_data)
