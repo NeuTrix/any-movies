70de33f4c198451f363ec5ebaa04f4c8034f02a8
NeuTrix <neutrix@brandgeneering.com>
2019-02-05 20:35:44 -0800

    Fix server issue with https
    Had migrated setupProxy and apihelper to use https
    vs http and broke the local connection
    
    Heroku deployed worked fine with this.
    
    Fixed error in the two files

--- client/src/helpers/api.helper.js
+++ client/src/helpers/api.helper.js
@@ -1,8 +1,8 @@
 // url for local host connection
-export const url_local_api = `https://localhost:3001`;
+export const url_local_api = `http://localhost:3001`;
 
 // OMDB Api connection
 // utilize CRA's Env variables (start with 'REACT_APP')
 export const apiKey = process.env.REACT_APP_OMBD_MOVIE_API_KEY
 
-export const url_movie_data = `https://www.omdbapi.com/?apikey=${apiKey}`;
+export const url_movie_data = `http://www.omdbapi.com/?apikey=${apiKey}`;
