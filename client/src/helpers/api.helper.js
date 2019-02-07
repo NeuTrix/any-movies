c71cf7964945a157db4ed54d5cfcbc17bc1e1627
NeuTrix <neutrix@brandgeneering.com>
2019-02-06 16:52:31 -0800

    rename omdb_url var

--- client/src/helpers/api.helper.js
+++ client/src/helpers/api.helper.js
@@ -1,8 +1,4 @@
-// url for local host connection
-export const url_local_api = 'http://localhost:3001';
-
 // OMDB Api connection
 // utilize CRA's Env variables (start with 'REACT_APP')
 export const apiKey = process.env.REACT_APP_OMBD_MOVIE_API_KEY;
-
-export const url_movie_data = `http://www.omdbapi.com/?apikey=${apiKey}`;
+export const omdb_url = `http://www.omdbapi.com/?apikey=${apiKey}`;
