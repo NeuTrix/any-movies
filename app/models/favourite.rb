7ff1f893d0c46eae07f528b9d89cdbe6e5ed49ec
NeuTrix <neutrix@brandgeneering.com>
2019-02-06 16:05:52 -0800

    refactor uniques method in Favs model

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -2,7 +2,8 @@ class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :favourited, polymorphic: true
   # ensure only 1 like per user per
-  validates_uniqueness_of :user_id, 
+  validates :user_id, uniqueness: {
     scope: [:favourited_id, :favourited_type],
     message: "You've already favourited this film!" 
+  }
 end
