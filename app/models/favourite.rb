4790676e86df387b0fbf1f8452d90683f24d9b5b
NeuTrix <neutrix@brandgeneering.com>
2019-02-06 20:47:22 -0800

    Model #index responds if a search item is requested
    Path either takes an arg (user_id, favourited_id) and verifies existance,
    or just defaults to returning the full index
    
    Flags is there is an arg missing,
    but need to generate an actual err message

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,3 +1,4 @@
+# for assigning favourites to movies (later comments and users)
 class Favourite < ApplicationRecord
   belongs_to :user
   belongs_to :favourited, polymorphic: true
@@ -6,4 +7,31 @@ class Favourite < ApplicationRecord
     scope: [:favourited_id, :favourited_type],
     message: "You've already favourited this film!" 
   }
+
+  def initialize
+    errors = ActiveModel::Errors.new(self)
+  end
+  # allows validation of existing favourite via #index/get
+  def self.search(search)
+
+    @uid = search[:user_id]
+    @fid = search[:favourited_id]
+
+    # not passing a :search symbol so need to check search values are present
+    # if at least one argument is present, validate the favourite
+    if @uid || @fid
+      # if only one argument is present, notify the issue
+      if !@uid || !@fid
+        "missing an argument (user_id or favourited_id)"
+      else
+        # returns a boolean value
+        self.exists?( user_id: @uid, favourited_id: @fid )
+      end
+    # if no search request submitted, return full #index, as normal
+    else
+      self.all
+    end
+
+  end
+
 end
