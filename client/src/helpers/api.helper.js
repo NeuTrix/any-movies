6a3f068505c5ef30bbbb90459f5d9b6786ec295b
NeuTrix <neutrix@brandgeneering.com>
2019-02-11 07:32:36 -0800

    add poster url helper

--- client/src/helpers/api.helper.js
+++ client/src/helpers/api.helper.js
@@ -2,3 +2,4 @@
 // utilize CRA's Env variables (start with 'REACT_APP')
 export const apiKey = process.env.REACT_APP_OMBD_MOVIE_API_KEY;
 export const omdb_url = `http://www.omdbapi.com/?apikey=${apiKey}`;
+export const omdb_poster_url = `http://img.omdbapi.com/?apikey=${apiKey}`;
