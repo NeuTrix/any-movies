aeafa7d84cf3837bba8a3d29a9fb21fa769bf3a5
NeuTrix <neutrix@brandgeneering.com>
2019-02-15 18:45:13 -0800

    Fix conflicts

--- client/src/components/movies/index.js
+++ client/src/components/movies/index.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export { default as MovieContainer } from './MovieContainer';
-=======
-export { default as MovieContainer } from './MovieContainer';
-t
->>>>>>> 2a687c5... rearrange folders with indexes
