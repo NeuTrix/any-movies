230e54a23d03d78d3e52de140505a4cd3024f1fd
NeuTrix <neutrix@brandgeneering.com>
2019-02-16 10:44:18 -0800

    tweaks

--- client/src/redux/combinedReducer.js
+++ client/src/redux/combinedReducer.js
@@ -2,7 +2,7 @@ import { combineReducer } from 'redux';
 import { currMovieReducer } from '../modules/movies/redux';
 
 const combinedReducers = combineReducer({
-  currMovie: currMovieReducer,
+	currMovie: currMovieReducer,
 });
 
 export default combinedReducers;
