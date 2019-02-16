18a075d34b77fbe25a74bd1b8fba3fbfc4f57812
NeuTrix <neutrix@brandgeneering.com>
2019-02-15 18:45:13 -0800

    tweaks

--- client/src/components/movies/index.js
+++ client/src/components/movies/index.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 export { default as MovieContainer } from './MovieContainer';
+=======
+export { default as MovieContainer } from './MovieContainer';
+t
+>>>>>>> 2a687c5... rearrange folders with indexes
