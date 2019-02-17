0d59b725049e054bcdb8094592296460ad0a32f6
NeuTrix <neutrix@brandgeneering.com>
2019-02-16 18:12:51 -0800

    favourites reducer linked

--- client/src/modules/movies/redux/index.js
+++ client/src/modules/movies/redux/index.js
@@ -1,4 +1,4 @@
-export { currMovieReducer } from './currMovieReducer';
+export { movieReducer } from './movieReducer';
 export { addMovie } from './moviesActions';
 // constants from strings to ensure against typos
 export { UPDATE_MOVIE } from './moviesConstants';
