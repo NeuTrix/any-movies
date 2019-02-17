a3baaa81b2881cdbfb5c58517a8fff380dfa05d9
NeuTrix <neutrix@brandgeneering.com>
2019-02-17 10:32:37 -0800

    begin 'getMovie"

--- client/src/modules/movies/redux/moviesActions.js
+++ client/src/modules/movies/redux/moviesActions.js
@@ -1,36 +1,50 @@
+import axios from 'axios';
+import { omdb_url } from '../../../helpers/api.helper'
 import { 
   FETCH_MOVIE_FAILURE,
   FETCH_MOVIE_REQUEST,
   FETCH_MOVIE_SUCCESS,
 } from './moviesConstants';
 
-import axios from 'axios';
 
 export function fetchMovieRequest(movieId) {
-  return {
-    type: FETCH_MOVIE_REQUEST,
-    payload: {
-      status: 'requested',
-    }
-  }
+	return {
+		type: FETCH_MOVIE_REQUEST,
+		payload: {
+			status: 'requested',
+		},
+	};
 }
 
 export function fetchMovieSuccess(movieId, data) {
-  return {
-    type: FETCH_MOVIE_SUCCESS,
-    payload: {
-      data,
-      status: 'successful',
-    }
-  }
+	return {
+		type: FETCH_MOVIE_SUCCESS,
+		payload: {
+			data,
+			status: 'successful',
+		},
+	};
 }
 
 export function fetchMovieFailure(movieId, error) {
-  return {
-    type: FETCH_MOVIE_FAILURE,
-    payload: {
-      status: 'errored',
-    }
+	return {
+		type: FETCH_MOVIE_FAILURE,
+		payload: {
+			status: 'errored',
+		},
+	};
+}
+
+export function getMovie(movieTitle) {
+  // using thunk middleware allows action to return a fn
+  return function(dispatch) {
+    // alert state of request action
+    dispatch(fetchMovieRequest(movieTitle))
+
+      // return the axios promise with the data/status 
+      return axios.get(`${omdb_url}`,  () => {
+
+      })
   }
 
 }
