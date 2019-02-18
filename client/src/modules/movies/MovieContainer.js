5c32d865abfe6443b882b5ea12cd36e9b64723a1
NeuTrix <neutrix@brandgeneering.com>
2019-02-17 19:27:26 -0800

    Amazing.  Redux got my Movie container down from 235 to 15 lines of code!
    What was I thinking.  Was worth the investment

--- client/src/modules/movies/MovieContainer.js
+++ client/src/modules/movies/MovieContainer.js
@@ -1,66 +1,7 @@
 // container to gather movie logic
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import shortid from 'shortid';
-import { omdbUrl } from '../../helpers/api.helper';
 import MoviePage from './MoviePage'
-
-import {
-	// addFavourite,
-	// getFavourites,
-	isFavourited,
-} from '../favourites';
-
-const propTypes = {
-	comments: PropTypes.instanceOf(Array).isRequired, // commentsfor current movie
-	currMovie: PropTypes.instanceOf(Object).isRequired, // movie under review
-	currUser: PropTypes.instanceOf(Object).isRequired, // current session user
-	isFormDisplayed: PropTypes.bool.isRequired, // status of comments form
-	isMovieRegistered: PropTypes.bool.isRequired, // movie registered in api db status
-};
-
-class MovieContainer extends Component {
-
-	constructor(props) {
-		super(props);
-		// set intial state for the application
-		this.state = {
-		};
-	}
-
-	// immutably set state with currUser props, movie data, and comments
-	componentDidMount() {
-	}
-
-	// update the component if new props recieved
-	componentDidUpdate(prevProps, prevState) {
-	}
-
-	render() {
-		// deconstruct state objects
-		const {
-			comments,
-			currMovie,
-			currUser,
-			isFormDisplayed,
-			isMovieRegistered,
-		} = this.props;
-
-		return (
-			<MoviePage
-        comments={comments}
-        currMovie={currMovie}
-        currUser={currUser}
-        isMovieRegistered={isMovieRegistered}
-        isFormDisplayed={isFormDisplayed}
-      />
-		);
-	}
-}
-
-MovieContainer.propTypes = propTypes;
+// import actions to dispatch to props as needed
 
 const mapStateToProps = state => ({
 	comments: state.comments.comments,
@@ -70,4 +11,6 @@ const mapStateToProps = state => ({
 	isMovieRegistered: state.movies.isMovieRegistered,
 });
 
-export default connect(mapStateToProps)(MovieContainer);
+const MovieContainer = connect(mapStateToProps)(MoviePage);
+
+export default MovieContainer
