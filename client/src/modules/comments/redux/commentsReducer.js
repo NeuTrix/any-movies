1c445acf0460e7172646b3c8283ee6c292f9afc6
NeuTrix <neutrix@brandgeneering.com>
2019-02-17 20:48:40 -0800

    Change var names from movies to comments

--- client/src/modules/comments/redux/commentsReducer.js
+++ client/src/modules/comments/redux/commentsReducer.js
@@ -1,4 +1,44 @@
-// reducer for user and movie comments
-export default function commentsReducer(state = {}, action = {}) {
- return state;
+// reducer for comments actions
+import {
+	FETCH_MOVIE_FAILURE,
+	FETCH_MOVIE_REQUEST,
+	FETCH_MOVIE_SUCCESS,
+	UPDATE_CURRENT_MOVIE,
+} from './commentsConstants';
+
+const initialState = {
+    currMovie: {
+      imdbID: 'tt0078748',
+      Title: 'Alien',
+    },
+    isFavourited: false, // change name to isMovieFavourited...
+	  isMovieRegistered: false,
+	  requestToOmdbApi: {
+			isFetching: false,
+      status: '',
+      message: '',
+	  },
+};
+
+export default function commentsReducer(state = initialState, action = {}) {
+	// deconstruct the action item
+	const { type, payload } = action;
+
+	switch (type) {
+	// request to the OMBD api
+  case FETCH_MOVIE_REQUEST:
+    return Object.assign({}, state, payload);
+
+	case FETCH_MOVIE_FAILURE:
+		return Object.assign({}, state, payload);
+
+  case FETCH_MOVIE_SUCCESS:
+		return Object.assign({}, state, payload);
+
+	case UPDATE_CURRENT_MOVIE:
+    return Object.assign({}, state, payload);
+
+	default:
+		return state;
+	}
 }
---
+++ client/src/modules/comments/redux/commentsReducer.test.js
@@ -0,0 +1,113 @@
+import { expect } from 'chai';
+import deepfreeze from 'deep-freeze';
+
+import {
+	FETCH_MOVIE_FAILURE,
+	FETCH_MOVIE_REQUEST,
+	FETCH_MOVIE_SUCCESS,
+	UPDATE_CURRENT_MOVIE,
+} from './commentsConstants';
+
+import {
+	fetchMovieRequest,
+	fetchMovieSuccess,
+	fetchMovieFailure,
+} from './commentsActions';
+
+import commentsReducer from './commentsReducer';
+
+const newMovie = {
+	imdb_id: 'tt0112431',
+	ratings: { metacritic: 100, tomatoes: 95 },
+	title: 'Babe',
+};
+
+describe('The Movies Reducer', () => {
+	let prevState; // initial prevState object for testing
+
+	beforeEach(() => {
+		prevState = { };
+		// deepfreeze // add to test for immutability
+		deepfreeze(prevState);
+	});
+
+	it('...undefined action returns default prevState', () => {
+		expect(commentsReducer(prevState)).to.eql(prevState);
+	});
+
+	
+});
+
+describe('MoviesReducer Async Actions', () => {
+	let prevState;
+
+	beforeEach(() => {
+		prevState = {
+			currMovie: {},
+			requestToOmdbApi: {
+				isFetching: false,
+				message: '',
+				status: 'pending',
+			},
+		};
+		deepfreeze(prevState);
+	});
+
+	describe('=> FETCH_MOVIE_REQUEST', () => {
+		const movieTitle = 'Babe';
+		const action = fetchMovieRequest(movieTitle);
+		const newState = commentsReducer(prevState, action);
+
+		it('... sets `requestToOmdbApi.isFetching` to be `true`', () => {
+			expect(newState.requestToOmdbApi.isFetching).to.eql(true);
+		});
+
+		it('... sets `requestToOmdbApi.status` to be `requesting`', () => {
+			expect(newState.requestToOmdbApi.status).to.eql('requesting');
+		});
+
+		it('... prevState had a `currMovie` prop', () => {
+			expect(prevState).to.have.property('currMovie')
+		});
+
+		it('... newState to have `currMovie` prop', () => {
+			expect(newState).to.have.property('currMovie')
+		});
+	});
+
+	describe('=> FETCH_MOVIE_SUCCESS', () => {
+		const action = fetchMovieSuccess(newMovie);
+		const newState = commentsReducer(prevState, action);
+
+		it('... sets `requestToOmdbApi.isFetching` to be `false`', () => {
+			expect(newState.requestToOmdbApi.isFetching).to.eql(false);
+		});
+
+		it('... sets `requestToOmdbApi.status` to be `success`', () => {
+			expect(newState.requestToOmdbApi.status).to.eql('success');
+		});
+
+		it('...updates the currMovie object', () => {
+			expect(newState.currMovie).to.eql(newMovie)
+		});
+	});
+
+	describe('=> FETCH_MOVIE_FAILURE', () => {
+		const error = 'A mock error message';
+		const action = fetchMovieFailure(error);
+		const newState = commentsReducer(prevState, action);
+
+		it('... sets `requestToOmdbApi.isFetching` to be`false`', () => {
+			expect(newState.requestToOmdbApi.isFetching).to.eql(false);
+		});
+
+		it('... sets `requestToOmdbApi.status` to be`error`', () => {
+			expect(newState.requestToOmdbApi.status).to.eql('error');
+		});
+
+		it('...updates the isFetching message with error data', () => {
+			expect(newState.requestToOmdbApi.message).to.eql(error);
+		});
+	});
+
+});
