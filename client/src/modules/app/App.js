ee3578a4f324c5c99449b3b778a67588e515ed1d
NeuTrix <neutrix@brandgeneering.com>
2019-02-18 08:19:15 -0800

    strip down app to pure function

--- client/src/modules/app/App.js
+++ client/src/modules/app/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import store from '../../redux/store';
 // pass in state to the MovieReveiwPage via Redux
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { FavouritesContainer } from '../favourites';
+// import { FavouritesContainer } from '../favourites';
 import masterTheme from './masterTheme';
-import { MoviePageContainer } from '../movies';
+// import { MoviePageContainer } from '../movies';
 import MainPage from './MainPage';
 
-
-class App extends Component {
-	render() {
-		return (
-			<Provider store={store} >
-				<MuiThemeProvider theme={masterTheme}>
-					<div className="App">
-						<MainPage/>
-						<FavouritesContainer />
-						<MoviePageContainer />
-					</div>
-				</MuiThemeProvider>
-			</Provider>
-		);
-	}
+function App() {
+	return (
+		<Provider store={store}>
+			<MuiThemeProvider theme={masterTheme}>
+				<div className="App">
+					<MainPage />
+					{/* <FavouritesContainer /> */}
+					{/* <MoviePageContainer /> */}
+				</div>
+			</MuiThemeProvider>
+		</Provider>
+	);
 }
 
 export default App;
