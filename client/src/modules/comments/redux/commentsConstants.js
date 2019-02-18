7d0c1869ee4f2e6be3f31f1fe8440f2f9367ff54
NeuTrix <neutrix@brandgeneering.com>
2019-02-17 21:25:42 -0800

    update comments constants names

---
+++ client/src/modules/comments/redux/commentsConstants.js
@@ -0,0 +1,8 @@
+// set immutable constants for movie actions
+
+// async actions
+export const FETCH_COMMENTS_FAILURE = 'FETCH_COMMENTS_FAILURE';
+export const FETCH_COMMENTS_REQUEST = 'FETCH_COMMENTS_REQUEST';
+export const FETCH_COMMENTS_SUCCESS = 'FETCH_COMMENTS_SUCCFUL';
+
+export const UPDATE_CURRENT_COMMENTS = 'UPDATE_CURRENT_COMMENTS';
