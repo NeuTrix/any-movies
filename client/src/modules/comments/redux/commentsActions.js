a72647d10f49742d624bfb55b09c712138ee9ff6
NeuTrix <neutrix@brandgeneering.com>
2019-02-19 14:15:23 -0800

    adjust variable names

--- client/src/modules/comments/redux/commentsActions.js
+++ client/src/modules/comments/redux/commentsActions.js
@@ -11,54 +11,54 @@ import {
 const comments = new schema.Entity('comments');
 const commentsSchema = ([comments]);
 
-export function fetchCommentsRequest(comId, comPath) {
+export function fetchCommentsRequest(commentableID, path) {
 	return {
 		type: FETCH_COMMENTS_REQUEST,
 		payload: {
-			requestToApi: {
+			apiRequest: {
 				isFetching: true,
-				message: `requesting comments for ${comPath} with id: ${comId}`,
+				message: `requesting comments for ${path} with id: ${commentableID}`,
 				status: 'requesting',
 			},
 		},
 	};
 }
 
-export function fetchCommentsSuccess(data, comId, comPath) {
+export function fetchCommentsSuccess(data, commentableID, path) {
 	return {
 		type: FETCH_COMMENTS_SUCCESS,
 		payload: {
 			data,
-			requestToApi: {
+			apiRequest: {
 				isFetching: false,
-				message: `recieved comments for ${comPath} with id: ${comId}`,
+				message: `recieved comments for ${path} with id: ${commentableID}`,
 				status: 'success',
 			},
 		},
 	};
 }
 
-export function fetchCommentsFailure(error, comId, comPath) {
+export function fetchCommentsFailure(error, commentableID, path) {
 	return {
 		type: FETCH_COMMENTS_FAILURE,
 		payload: { 
-			requestToApi: {
+			apiRequest: {
 				isFetching: false, 
-				message: `Error getting comments for ${comPath} with id: ${comId} \n ${error}`,
+				message: `Error getting comments for ${path} with id: ${commentableID} \n ${error}`,
 				status: 'error' },
 		},
 	};
 }
 
-// `comId` maps to commentable_id and `comPath` to `classses` or `movies` paths
-export function getComments(comId, comPath) {
+// `commentableID` maps to commentable_id and `path` to `classses` or `movies` paths
+export function getComments(commentableID, path) {
 	// using thunk middleware to return a fn from an action
 	// named it `thunk` to clear linting err re:anonymous fucntions
 	return function thunk(dispatch) {
 		// alert state of request action
-		dispatch(fetchCommentsRequest(comId, comPath));
+		dispatch(fetchCommentsRequest(commentableID, path));
 		// return the axios promise with the data/status
-		return axios.get(`/api/${comPath}/${comId}/comments`)
+		return axios.get(`/api/${path}/${commentableID}/comments`)
 			// normalize the response data
 			.then(resp => normalize(resp.data, commentsSchema))
 			// inspect the normalized data
@@ -66,9 +66,9 @@ export function getComments(comId, comPath) {
 				console.log('--comments-->', data);
 				return data;
 			})
-			.then(data => dispatch(fetchCommentsSuccess(data, comId, comPath)))
+			.then(data => dispatch(fetchCommentsSuccess(data, commentableID, path)))
 			.catch((err) => {
-				dispatch(fetchCommentsFailure(err, comId, comPath));
+				dispatch(fetchCommentsFailure(err, commentableID, path));
 				console.log('---getComments--->', err);
 			});
 	};
