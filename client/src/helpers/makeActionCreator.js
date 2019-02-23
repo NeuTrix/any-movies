19b607d4bfc3fe92bca945ca0f27b25cc20404f9
NeuTrix <neutrix@brandgeneering.com>
2019-02-23 14:15:59 -0800

    Updated make action creator function to accept /construct payloads

--- client/src/helpers/makeActionCreator.js
+++ client/src/helpers/makeActionCreator.js
@@ -1,15 +1,17 @@
 // generates an action creator function, minimizing boilerplate
 // 'type' is required by convention
 // 'names' are the names of the additional values passed to the action
-export default function makeActionCreator(type, ...names) {
+export default function makeActionCreator(type, ...payloadPropertyNames) {
 	return function newAction(...args) {
-		const action = { type };
+		// returns a paylod object as part of the action
+		const action = { type, payload: {} };
 
-		names.forEach((prop, index) => {
-			action[prop] = args[index];
+		payloadPropertyNames.forEach((prop, index) => {
+			action.payload[prop] = args[index];
 		});
 
 		console.log('Created a new action object', action);
 		return action;
 	};
 }
+ 
--- client/src/helpers/makeActionCreator.test.js
+++ client/src/helpers/makeActionCreator.test.js
@@ -1,44 +1,61 @@
 import { expect } from 'chai';
 import { makeActionCreator } from '.';
 
-describe('THe makeActionCreator function', () => {
+describe('The makeActionCreator function', () => {
+	// create the new action creator function
+	// assign args to variables for testing comparisons
 	const type = 'PEANUT_BUTTER';
 	const madeArg1 = 'brand';
 	const madeArg2 = 'style';
 	const madeAction = makeActionCreator(type, madeArg1, madeArg2);
 
-	it('...is a function', () => {
-		expect(makeActionCreator).to.be.a('function');
+	// generate an action object from the new function
+	const actionArg1 = 'Jiffy';
+	const actionArg2 = 'crunchy';
+	const action = madeAction(actionArg1, actionArg2);
+
+	describe('--> The new action creator', () => {
+		it('...is a function', () => {
+			expect(makeActionCreator).to.be.a('function');
+		});
+		it('...returns a function', () => {
+			expect(madeAction).to.be.a('function');
+		});
 	});
 
-	it('...returns a function', () => {
-		expect(madeAction).to.be.a('function');
-	});
-
-	describe('...the returned function', () => {
-		// generate an action object
-		const actionArg1 = 'Jiffy';
-		const actionArg2 = 'crunchy';
-		const action = madeAction(actionArg1, actionArg2);
-
-		it('...creates an action object', () => {
+	describe('...the returned action object', () => {
+		it('...is an object returned from the new function', () => {
 			expect(action).to.be.an('object');
 		});
 
-		it('...with the expected type property', () => {
+		it('... has only two propertie', () => {
+			expect(Object.keys(action).length).to.eql(2);
+		});
+
+		it('...has the expected type properties', () => {
 			expect(action).to.have.property('type')
 				.to.eql(type); // the test variable
 		});
 
+		it('...has a payload object property', () => {
+			expect(action).to.have.property('payload')
+				.to.be.an('object');
+		});
+	});
+
+
+	describe.only('--> The payload object', () => {
+		const { payload } = action;
+
 		it('...has the correct number of additional args', () => {
-			console.log(Object.keys(action));
-			expect(Object.keys(action).length).to.eql(3);
+			console.log(Object.keys(payload));
+			expect(Object.keys(payload).length).to.eql(2);
 		});
 
 		it('...has the correct prop names', () => {
-			expect(action).to.have.property(madeArg1)
+			expect(payload).to.have.property(madeArg1)
 				.to.eql(actionArg1);
-			expect(action).to.have.property(madeArg2)
+			expect(payload).to.have.property(madeArg2)
 				.to.eql(actionArg2);
 		});
 	});
