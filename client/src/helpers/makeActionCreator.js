1332ea5775bcff4a9e9b7e067d702faf3ac3e102
NeuTrix <neutrix@brandgeneering.com>
2019-02-23 13:47:52 -0800

    tweaks

--- client/src/helpers/makeActionCreator.js
+++ client/src/helpers/makeActionCreator.js
@@ -2,14 +2,13 @@
 // 'type' is required by convention
 // 'names' are the names of the additional values passed to the action
 export default function makeActionCreator(type, ...names) {
-
 	return function newAction(...args) {
 		const action = { type };
 
 		names.forEach((prop, index) => {
 			action[prop] = args[index];
-    });
-    
+		});
+
 		console.log('Created a new action object', action);
 		return action;
 	};
