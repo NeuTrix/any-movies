bd9538ded358742366f8e5a91120bf647bc5f7be
NeuTrix <neutrix@brandgeneering.com>
2019-02-24 10:39:14 -0800

    Linting

--- client/src/modules/movies/redux/moviesActions.js
+++ client/src/modules/movies/redux/moviesActions.js
@@ -1,55 +1,44 @@
+
+import axios from 'axios';
+import { normalize, schema } from 'normalizr';
 import {
 	FETCH_MOVIE_FAILURE,
 	FETCH_MOVIE_REQUEST,
 	FETCH_MOVIE_SUCCESS,
 } from './moviesConstants';
-
-import axios from 'axios';
-import { normalize, schema } from 'normalizr';
 import { actionCreator, omdbUrl } from '../../helpers';
 
 // normalizr schema
 export const movie = new schema.Entity('movies', {}, { idAttribute: 'imdbID' });
 
-export const fetchMovieRequest = actionCreator(FETCH_MOVIE_REQUEST)
-
-export const fetchMovieSuccess = actionCreator(
-	FETCH_MOVIE_SUCCESS, 
-	'movieID', 
-	'dictionary'
-); 
-
+// ==> Build actions with actionCreator. Simplifies boilerplate
+// track progress of api request to OMDB database
+export const fetchMovieRequest = actionCreator(FETCH_MOVIE_REQUEST);
 // captures the error messages on fail
-export const fetchMovieFailure = actionCreator(
-	FETCH_MOVIE_FAILURE,
-	'error',
+export const fetchMovieFailure = actionCreator(FETCH_MOVIE_FAILURE, 'error');
+// logs success of api call and returns movie ID and detailed object/dictionary
+export const fetchMovieSuccess = actionCreator(
+	FETCH_MOVIE_SUCCESS,
+	'movieID',
+	'dictionary',
 );
 
-// set the current movie
-// export function setCurrentComment(commentableID) {
-// 	return {
-// 		type: SET_CURRENT_MOVIE,
-// 		payload: {
-// 			commentableID,
-// 		},
-// 	};
-// }
-
+// Redux thunk to facilitate async actions (not tested)
 export function getMovieData(movieTitle) {
 	// using thunk middleware to return a fn from an action
 	// named it `thunk` to clear linting err re:anonymous fucntions
 	return function thunk(dispatch) {
-		// alert state of request action
+		// alert app of request action
 		dispatch(fetchMovieRequest(movieTitle));
 		// return the axios promise with the data/status
 		return axios.get(`${omdbUrl}&t=${movieTitle}`)
 			// normalize the data
 			.then(resp => normalize(resp.data, movie))
-			.then(data => {
+			// pass normalize data to the application
+			.then((data) => {
 				const movieID = data.result;
 				const dictionary = data.entities.movies;
-				console.log(1, '==>', movieID, dictionary);
-				dispatch(fetchMovieSuccess(movieID, dictionary))
+				dispatch(fetchMovieSuccess(movieID, dictionary));
 			})
 			.catch((err) => {
 				dispatch(fetchMovieFailure(err));
