7e6eee8807c36f91085e22abb0d52b8283a282a4
NeuTrix <neutrix@brandgeneering.com>
2019-02-23 17:45:38 -0800

    Begin refactor of MovieReducerTests
    copied from Comments

--- client/src/modules/movies/redux/moviesReducer.test.js
+++ client/src/modules/movies/redux/moviesReducer.test.js
@@ -1,165 +1,161 @@
-import { expect } from 'chai';
+
 import deepfreeze from 'deep-freeze';
+import { expect } from 'chai';
 
+// action creators
 import {
-	fetchMovieFailure,
-	fetchMovieRequest,
-	fetchMovieSuccess,
-	setCurrentMovie,
-} from './moviesActions';
+	fetchCommentsFailure,
+	fetchCommentsRequest,
+	fetchCommentsSuccess,
+	setCommentable,
+} from './commentsActions';
 
-import moviesReducer, {
-	initialState,
-} from './moviesReducer';
+// test objects
+import {
+	comments1,
+	comments2,
+	dictionary1,
+	dictionary2,
+} from './commentsTestHelper';
 
-const data1 = {
-	Ratings: { metacritic: 100, tomatoes: 95 },
-	Title: 'Babe',
-	imdbID: 'tt0112431',
-};
-
-const data2 = {
-	Ratings: { metacritic: 95, tomatoes: 85 },
-	Title: 'Alien',
-	imdbID: 'tt0078748',
-};
+import commentsReducer, { initialState } from './commentsReducer';
 
 // ensure an immutable previous state object for tests
 const prevState = initialState;
 deepfreeze(prevState);
 
-describe('Movies reducer core actions', () => {
+describe('Comments reducer core actions', () => {
 	it('...undefined action returns default previous state', () => {
-		expect(moviesReducer(prevState)).to.eql(prevState);
+		expect(commentsReducer(prevState)).to.eql(prevState);
 	});
 });
 
-describe('The FETCH_MOVIE_SUCCESS action', () => {
-	const action = fetchMovieSuccess(data1);
-	const nextState = moviesReducer(prevState, action);
-	const action2 = fetchMovieSuccess(data2);
-	const nextState2 = moviesReducer(nextState, action2);
+describe('The FETCH_COMMENTS_SUCCESS action', () => {
+	const action = fetchCommentsSuccess(comments1, dictionary1);
+	const nextState = commentsReducer(prevState, action);
+	const action2 = fetchCommentsSuccess(comments2, dictionary2);
+	const nextState2 = commentsReducer(nextState, action2);
 
 	describe('The nextState properties', () => {
-		xit('--> nextState has `apiRequest` prop', () => {
+		it('--> nextState has `apiRequest` prop', () => {
 			expect(nextState).to.have.property('apiRequest')
 				.to.be.an('object');
 		});
 
-		xit('--> nextState has a commentIndexes (array) prop', () => {
-			expect(nextState).to.have.property('commentIndexes')
+		it('--> nextState has a subComments (array) prop', () => {
+			expect(nextState).to.have.property('subComments')
 				.to.be.an('array');
 		});
 
-		xit('...movies array has the correct value', () => {
-			expect(nextState.movies[0]).to.eql(data1.imdbID);
+		it('...subComments array has the correct value', () => {
+			expect(nextState.subComments[0]).to.eql(comments1[0]);
 		});
 
-		xit('...movies array length incremented properly', () => {
-			expect(prevState.movies.length).to.eql(0);
-			expect(nextState.movies.length).to.eql(1);
+		it('...subComments array length incremented properly', () => {
+			expect(prevState.subComments.length).to.eql(0);
+			expect(nextState.subComments.length).to.eql(1);
 		});
 
-		xit('--> nextState has a `current` prop', () => {
-			expect(nextState).to.have.property('current')
+		it('--> nextState has a `commentable` prop', () => {
+			expect(nextState).to.have.property('commentable')
 				.to.be.an('string');
 		});
 
-		xit('...nextState has `favourited` prop', () => {
+		it('...nextState has `favourited` prop', () => {
 			expect(nextState).to.have.property('favourited')
 				.to.be.an('boolean');
 		});
 
-		xit('...nextState has `showForm` prop', () => {
+		it('...nextState has `showForm` prop', () => {
 			expect(nextState).to.have.property('showForm')
 				.to.be.an('boolean');
 		});
 	});
 
 	describe('The apiRequest object', () => {
-		xit('... has an updated apiRequest.isFetching prop', () => {
+		it('... has an updated apiRequest.isFetching prop', () => {
 			expect(nextState.apiRequest).to.have.property('isFetching')
 				.to.eql(false);
 		});
 
-		xit('... has an apiRequest.message prop', () => {
+		it('... has an apiRequest.message prop', () => {
 			expect(nextState.apiRequest).to.have.property('message');
 		});
 
-		xit('... has an updated apiRequest.status', () => {
+		it('... has an updated apiRequest.status', () => {
 			expect(nextState.apiRequest).to.have.property('status')
 				.to.eql('success');
 		});
 	});
 
 	describe('The dictionary prop | sub reducer', () => {
-		xit('...has initial dictionary of length 0', () => {
+		it('...has initial dictionary of length 0', () => {
 			expect(Object.keys(prevState.dictionary).length).to.eql(0);
 		});
 
-		xit('...has nextState 1 dictionary of length 1', () => {
+		it('...has nextState 1 dictionary of length 1', () => {
 			expect(Object.keys(nextState.dictionary).length).to.eql(1);
 		});
 
-		xit('...has nextState 2 dictionary of length 3', () => {
+		it('...has nextState 2 dictionary of length 3', () => {
 			expect(Object.keys(nextState2.dictionary).length).to.eql(3);
 		});
 	});
 });
 
-describe('The FETCH_MOVIE_REQUEST', () => {
-	const action = fetchMovieRequest();
-	const nextState = moviesReducer(prevState, action);
+describe('The FETCH_COMMENTS_REQUEST', () => {
+	const action = fetchCommentsRequest();
+	const nextState = commentsReducer(prevState, action);
 
 	describe('... the apiRequest object', () => {
-		xit('... has an updated apiRequest.isFetching prop', () => {
+		it('... has an updated apiRequest.isFetching prop', () => {
 			expect(nextState.apiRequest).to.have.property('isFetching')
 				.to.eql(true);
 		});
 
-		xit('... has an apiRequest.message prop', () => {
+		it('... has an apiRequest.message prop', () => {
 			expect(nextState.apiRequest).to.have.property('message');
 		});
 
-		xit('... has an updated apiRequest.status', () => {
+		it('... has an updated apiRequest.status', () => {
 			expect(nextState.apiRequest).to.have.property('status')
 				.to.eql('requesting');
 		});
 	});
 });
 
-describe('The FETCH_MOVIE_FAILURE action', () => {
+describe('The FETCH_COMMENTS_FAILURE action', () => {
 	const error = 'A mock error message';
-	const action = fetchMovieFailure(error);
-	const nextState = moviesReducer(prevState, action);
+	const action = fetchCommentsFailure(error);
+	const nextState = commentsReducer(prevState, action);
 
 	describe('... the apiRequest object', () => {
-		xit('... has an updated apiRequest.isFetching prop', () => {
+		it('... has an updated apiRequest.isFetching prop', () => {
 			expect(nextState.apiRequest).to.have.property('isFetching')
 				.to.eql(false);
 		});
 
-		xit('... has an apiRequest.message prop', () => {
+		it('... has an apiRequest.message prop', () => {
 			expect(nextState.apiRequest).to.have.property('message');
 		});
 
-		xit('... has an updated apiRequest.status', () => {
+		it('... has an updated apiRequest.status', () => {
 			expect(nextState.apiRequest).to.have.property('status')
 				.to.eql('error');
 		});
 	});
 });
 
-xdescribe('The SET_COMMENTABLE action', () => {
-	const action = setCurrentMovie(data1.imdbID);
-	const nextState = moviesReducer(prevState, action);
+describe('The SET_COMMENTABLE action', () => {
+	const action = setCommentable(comments1[0]);
+	const nextState = commentsReducer(prevState, action);
 
-	xit('...has an updated current prop', () => {
-		expect(nextState).to.have.property('current')
+	it('...has an updated commentable prop', () => {
+		expect(nextState).to.have.property('commentable')
 			.to.be.an('string');
 	});
 
-	xit('...has the expected value', () => {
-		expect(nextState.current).to.eql(data1.imdbID);
+	it('...has the expected value', () => {
+		expect(nextState.commentable).to.eql(comments1[0]);
 	});
 });
