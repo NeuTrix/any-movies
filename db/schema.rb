bd24be621f1cde37a7eaddd5de02b082e8cda83a
NeuTrix <neutrix@brandgeneering.com>
2019-03-03 11:49:45 -0800

    Add subcomments field to Movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_220821) do
+ActiveRecord::Schema.define(version: 2019_03_03_194740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_220821) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.string "imdb_id", null: false
+    t.text "sub_comments"
   end
 
   create_table "users", force: :cascade do |t|
