07128cd5007e436496d24615e484bda1eb53353e
NeuTrix <neutrix@brandgeneering.com>
2019-03-09 19:11:33 -0800

    Add edit and remove get comments fns

--- client/src/modules/comments/CommentableFormContainer.js
+++ client/src/modules/comments/CommentableFormContainer.js
@@ -1,17 +1,18 @@
 import { connect } from 'react-redux';
-import { addComment, getComments } from './redux/commentsActions';
+import { addComment, editComment } from './redux/commentsActions';
 import CommentableForm from './CommentableForm';
 
 const mapStateToProps = (state, props) => ({
   commentableID: props.commentableID,
   commentableType: props.commentableType,
+  // allow the form to toggle on submit (based on parent state)
   toggleForm: props.toggleForm,
   user: state.users.current,
 });
 
 const mapDispatchToProps = dispatch => ({
   addComment: data => dispatch(addComment(data)),
-  getComments: () => dispatch(getComments()),
+  editComment: data => dispatch(editComment(data)),
 });
 
 const CommentableFormContainer = connect(mapStateToProps, mapDispatchToProps)(CommentableForm);
