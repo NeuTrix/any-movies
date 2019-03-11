bfe5b295472547dbb68bc0048742c53e0b3ff949
NeuTrix <neutrix@brandgeneering.com>
2019-03-11 12:32:36 -0700

    tweak

--- client/src/modules/app/App.js
+++ client/src/modules/app/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import store from '../../redux/store';
-// pass in state to the MovieReveiwPage via Redux
-// import { FavouritesContainer } from '../favourites';
 import masterTheme from './masterTheme';
-// import { MoviePageContainer } from '../movies';
 import { MainPageContainer } from '../main';
 import { MenuBarContainer } from '../main'
 
