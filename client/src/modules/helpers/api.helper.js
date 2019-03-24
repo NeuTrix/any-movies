078bda11f151e86951d2917ff58348dd2f2aab9e
NeuTrix <neutrix@brandgeneering.com>
2019-03-24 13:56:57 -0700

    HTTPS  change local host and OMDB calls to https
    Attempting to fix netwrok error calls in the Heroku deployment

--- client/src/modules/helpers/api.helper.js
+++ client/src/modules/helpers/api.helper.js
@@ -2,5 +2,5 @@
 // OMDB Api connection
 // utilize CRA's Env variables (start with 'REACT_APP')
 export const apiKey = process.env.REACT_APP_OMBD_MOVIE_API_KEY;
-export const omdbUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
-export const omdbPosterUrl = `http://img.omdbapi.com/?apikey=${apiKey}`;
+export const omdbUrl = `https://www.omdbapi.com/?apikey=${apiKey}`;
+export const omdbPosterUrl = `https://img.omdbapi.com/?apikey=${apiKey}`;
