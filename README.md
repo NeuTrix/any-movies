583209dd339b2aa2860aa4877302d1d8a4e050e6
NeuTrix <neutrix@brandgeneering.com>
2019-03-26 00:41:31 -0700

    Update read me and tweaks

--- README.md
+++ README.md
@@ -28,5 +28,6 @@ Things you may want to cover:
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
-
+  # Heroku
+  - `heroku run rake db:migrate` to update the database
 * ...
--- client/src/modules/movies/MoviePage.js
+++ client/src/modules/movies/MoviePage.js
@@ -45,11 +45,10 @@ class MoviePage extends Component {
     const ratings = movie.Ratings && movie.Ratings.map(rating => (
       <div key={movie.Source} className={classes.ratingUnit}>
         <div style={{ gridArea: 'critic' }}>{`${rating.Source} :`}</div>
-        
+
         <div style={{ gridArea: 'grade', textAlign: 'right' }}>
           { rating.Value }
         </div>
-
       </div>
     ));
 
